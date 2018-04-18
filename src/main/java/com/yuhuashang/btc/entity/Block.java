3b833de50a26052066e6457faf03167f6fda95ba
yuhuashang <yuhuashang@aliyun.cfom>
2018-04-18 13:54:49 +0800

    Initial commit

---
+++ src/main/java/com/yuhuashang/btc/entity/Block.java
@@ -0,0 +1,19 @@
+package com.yuhuashang.btc.entity;
+
+public class Block {
+
+    /**
+     * 区块索引号
+     */
+    private int index;
+
+    /**
+     * 当前区块的hash值，区块唯一标识
+     */
+    private String hash;
+
+    /**
+     * 生成区块的时间戳
+     */
+    
+}
